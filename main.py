92d612a7df493055103a768fe0b447fff631923a
yellowpics <yellowpics613@gmail.com>
2025-02-18 23:59:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -147,4 +147,5 @@ def home():
 
 # ====== 🚀 LANCEMENT SERVEUR ======
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+
